2cdb0669237c5e20b0fbfc5493a024b592ad23f6
franl08 <a93202@alunos.uminho.pt>
2021-03-05 11:13:55 +0000

    Fixed bug and improve complexity of SumDates.java

--- Ficha01/ExerciciosI/SumDates.java
+++ Ficha01/ExerciciosI/SumDates.java
@@ -3,25 +3,21 @@ package Ficha01.ExerciciosI;
 public class SumDates{
 
     public static void sumDates(int day1, int hour1, int min1, int sec1, int day2, int hour2, int min2, int sec2){
-        int finalDay, finalHour, finalMin, finalSec;
-        finalDay = finalHour = finalMin = 0;
-        finalSec = sec1 + sec2;
-         while (finalSec > 59){
-            finalMin++;
-            finalSec = finalSec - 60;
-        }
-        finalMin = finalMin + min1 + min2;
-        while (finalMin > 59){
-            finalHour++;
-            finalMin = finalMin - 60;
-        }
-        finalHour = finalHour + hour1 + hour2;
-        while (finalHour > 23){
-            finalDay++;
-            finalHour = finalHour - 24;
-        }
-        finalDay = finalDay + day1 + day2;
-        System.out.println(finalDay + "D " + finalHour + "H " + finalMin + "M " + finalSec + "S ");
+        sec1 += sec2;
+        min1 += sec1 / 60;
+        sec1 = sec1 % 60;
+
+        min1 += min2;
+        hour1 += min1 / 60;
+        min1 = min1 % 60;
+
+        hour1 += hour2;
+        day1 += hour1 / 24;
+        hour1 = hour1 / 24;
+
+        day1 += day2;
+
+        System.out.println(day1 + "D " + hour1 + "H " + min1 + "M " + sec1 + "S ");
     }
     public static void main(String[] args){
        int day1 = Integer.parseInt(args[0]);
@@ -31,7 +27,7 @@ public class SumDates{
        int day2 = Integer.parseInt(args[4]);
        int hour2 = Integer.parseInt(args[5]);
        int min2 = Integer.parseInt(args[6]);
-       int sec2 = Integer.parseInt(args[5]);
+       int sec2 = Integer.parseInt(args[7]);
        sumDates(day1, hour1, min1, sec1, day2, hour2, min2, sec2);
     }
 }
