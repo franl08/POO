2a5a0beecd379dac10b976be4bd5408ec684e039
franl08 <a93202@alunos.uminho.pt>
2021-03-05 12:04:55 +0000

    Fixed to use printf in Tri.java

--- Ficha01/ExerciciosI/Tri.java
+++ Ficha01/ExerciciosI/Tri.java
@@ -3,23 +3,22 @@ import java.util.Scanner;
 
 public class Tri {
     public static void per (double base, double height){
-        double hip = Math.sqrt((base * base) + (height * height));
-        double per = (base + height + hip);
-        per = (double)Math.round(per * 100000d) / 100000d;
-        System.out.println("Perimeter: " + per);
+        float hip = Math.sqrt((base * base) + (height * height));
+        float per = (base + height + hip);
+        System.out.printf("Perimeter: %.5f%n", per);
+        
     }
 
     public static void area (double base, double height){
-        double area = (base * height) / 2;
-        area = (double)Math.round(area * 100000d) / 100000d;
-        System.out.println("Area: " + area);
+        float area = (base * height) / 2;
+        System.out.printf("Area: %.5f%n", area);
     }
     public static void main (String[] args){
         Scanner tri = new Scanner(System.in);
         while (true){
             System.out.println("Insert Base and Height: ");
-            double base = tri.nextDouble();
-            double height = tri.nextDouble();
+            float base = tri.nextFloat();
+            float height = tri.nextFloat();
             if (base != 0){
                 per(base, height);
                 area(base, height);
